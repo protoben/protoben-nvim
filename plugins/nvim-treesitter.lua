78979d559005b1eaddd8e2950890e724231a7452
Ben Hamlin <hamlinb@galois.com>
2026-05-01 11:49:13 -0700

    Update treesitter to check for neovim 0.12.0 and use main, not master

--- plugins/nvim-treesitter.lua
+++ plugins/nvim-treesitter.lua
@@ -1,28 +1,42 @@
 -- vim: ts=2 sw=2 expandtab
 
-return {
-  {
-    'nvim-treesitter/nvim-treesitter',
-    lazy = false,
-    opts = {
-      ensure_installed = {
-        'c',
-        'lua',
-        'vim',
-        'rust',
-        'python',
-        'bash',
-        'elixir',
-        'eex',
-        'heex',
-        'yaml',
-        'lua',
-        'zig',
-        'javascript',
-        'typescript',
-        'markdown',
-        'html',
-      },
-    }
-  },
+local langs = {
+  'c',
+  'lua',
+  'vim',
+  'rust',
+  'python',
+  'bash',
+  'elixir',
+  'eex',
+  'heex',
+  'yaml',
+  'lua',
+  'zig',
+  'javascript',
+  'typescript',
+  'markdown',
+  'html',
 }
+
+if vim.version().minor < 12 then
+  return {
+    {
+      'nvim-treesitter/nvim-treesitter',
+      branch = 'master',
+      lazy = false,
+      opts = {
+        ensure_installed = langs,
+      }
+    },
+  }
+else
+  return {
+      'nvim-treesitter/nvim-treesitter',
+      branch = 'main',
+      lazy = false,
+      setup = function()
+        require('nvim-treesitter').install(langs)
+      end
+  }
+end
